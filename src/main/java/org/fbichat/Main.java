b14c50145996f572104bf28fdad4549f157a11e9
Lucas <lucasmag97@gmail.com>
2021-02-20 22:43:52 -0300

    stop application on close

--- src/main/java/org/fbichat/Main.java
+++ src/main/java/org/fbichat/Main.java
@@ -1,5 +1,6 @@
 package org.fbichat;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -17,6 +18,10 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add("org/fbichat/styles.css");
 
+        stage.setOnCloseRequest(e -> {
+            Platform.exit();
+            System.exit(0);
+        });
         stage.setTitle("FBI Chat");
         stage.setScene(scene);
         stage.show();
